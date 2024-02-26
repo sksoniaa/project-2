d5cec25fa3fed552b3d5b3ebe983f3a211c18b9e
Sofiia Kosachevych <sofiakosachvych@gmail.com>
2024-02-26 13:23:12 -0600

    setup up oauth and User Model

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,7 @@ const passport = require('passport');
 
 // The root route renders our only view
 router.get('/', function(req, res) {
-  //UPDATE THIS ==========================================================================
-  // Where do you want to go for the root route (this is localhost:3000)
-  // in the student demo this was res.redirect('/movies'), what do you want?
-  // This could be a landing page, or just redirect to your main resource page which you'll have an a tag that makes 
-  // a request to `/auth/google` route below
-  //===============================================================================================
+  res.redirect('/recipes') 
 });
 
 // Google OAuth login route
@@ -21,15 +16,15 @@ router.get('/auth/google', passport.authenticate(
 router.get('/oauth2callback', passport.authenticate(
   'google',
   {
-    successRedirect : '/', // UPDATE THIS, where do you want the client to go after you login 
-    failureRedirect : '/' //  UPDATE THIS, where do you want the client to go if login fails
+    successRedirect : '/recipes', // UPDATE THIS, where do you want the client to go after you login 
+    failureRedirect : '/recipes' //  UPDATE THIS, where do you want the client to go if login fails
   }
 ));
 
 // OAuth logout route
 router.get('/logout', function(req, res){
   req.logout(function(){ //< - req.logout comes from passport, and what it does is destorys the cookie keeping track of the user!
-    res.redirect('/') // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
+    res.redirect('/recipes') // <---- UPDATE THIS TO WHERE YOU WANT THE USER TO GO AFTER LOGOUT
   })
 })
 
