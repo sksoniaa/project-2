5db12d7dc370470dfffe95d26674440c370dbaa7
Sofiia Kosachevych <sofiakosachvych@gmail.com>
2024-02-28 15:54:55 -0600

    user make changes to the recipe if they're not logged in, user can update comments

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,9 +1,33 @@
-const recipe = require('../models/recipe')
 const RecipeModel = require('../models/recipe')
 
 module.exports = {
   create,
-  delete: deleteComment
+  delete: deleteComment,
+  edit,
+  update
+}
+
+async function update(req, res) {
+  const recipeDoc = await RecipeModel.findOne({'comments._id': req.params.id});
+  console.log(recipeDoc, "<-- recipeDoc");
+  const commentSubdoc = recipeDoc.comments.id(req.params.id);
+  if (!commentSubdoc.user.equals(req.user._id)) return res.redirect(`/recipes/${recipeDoc._id}`);
+  // Update the text of the comment
+  console.log(req.body);
+  commentSubdoc.content = req.body.content;
+  try {
+    await recipeDoc.save();
+  } catch (e) {
+    console.log(e.message);
+  }
+  // Redirect back to the book's show view
+  res.redirect(`/recipes/${recipeDoc._id}`);
+}
+
+async function edit(req, res) {
+   const recipeDoc = await RecipeModel.findOne({'comments._id': req.params.id});
+   const comment = recipeDoc.comments.id(req.params.id);
+   res.render('recipes', { comment });
 }
 
 async function deleteComment(req,res) {
--- routes/comments.js
+++ routes/comments.js
@@ -4,5 +4,8 @@ const commentsCtrl = require('../controllers/comments')
 
 router.post('/recipes/:id/comments', commentsCtrl.create)
 router.delete('/comments/:id', commentsCtrl.delete)
+router.get('/recipes/:id/comments', commentsCtrl.edit)
+router.put('/comments/:id', commentsCtrl.update)
+
 
 module.exports = router;
